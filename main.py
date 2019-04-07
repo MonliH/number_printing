0a3bc03457a43a0659beb8d485a43f269475391e
Jonathan Li <jonathanliworld@gmail.com>
2019-04-07 14:48:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
-def printDigits(integer):
-    num=str(integer) #turn the integer into a string
-    if num=='': #base case: there is no number in the certain string
+
+def print_digits(integer):
+    num=str(integer) # turn the integer into a string
+    if num=='': # base case: there is no number in the certain string
+        print()
+        print()
         return
     else:
-        print(num[-1]) #print the last number first
-        printDigits(num[:-1])
+        print(num[-1], end="") # print the last number first
+        print_digits(num[:-1])
         return
 
-def evenOrOdd(number): #don't use recursion
-    if number%2==1:
+def even_or_odd(number): # don't use recursion
+    if number % 2 == 1:
         return False
-    elif number%2==0:
+    elif number % 2 == 0:
         return True
 
-def oddOrEven(number): #use recursion
+def odd_or_even(number): # use recursion
     if number==0:
         return True
     elif number==1:
         return False
     else:
         if number>0:
-            return oddOrEven(number-2)
+            return odd_or_even(number-2)
         else:
-            return oddOrEven(number+2)
-        
-def printAsterisks(n):
+            return odd_or_even(number+2)
+
+def print_asterisks(n):
     if n==1: #base case: only one asterisk in the first line
         return '*'
     else:
-        return printAsterisks(n-1)+'\n'+'*'*n #remember to change the line every time finishing printing one line of asterisks
+        return print_asterisks(n-1)+'\n'+'*'*n #remember to change the line every time finishing printing one line of asterisks
 
-def raisePower(num,exponent): #num**power=num*num**(power-1)
+def raise_power(num,exponent): #num**power=num*num**(power-1)
     if exponent==0: #base case: when power equals to 0
         return 1
     else:
-        return num*raisePower(num,exponent-1)
+        return num*raise_power(num,exponent-1)
 
 def menu():
     #print the menu
@@ -45,32 +48,35 @@ def menu():
     print('(5)Raise a number to a certain power.')
     print('(6)Exit')
     #Let the user give option
-    function=input('What function do you want to run?(Hint: Enter 1 or 2 or 3 or 4 or 5 or 6)')
+    function = str(input('\nWhat function do you want to run?(Hint: Enter 1 or 2 or 3 or 4 or 5 or 6)'))
     if function=='1':
         integer=input('What integer do you want to reverse?')
-        newInteger=printDigits(integer)
+        newInteger=print_digits(integer)
         menu() #run the function again
     elif function=='2':
         number=int(input('What is the number you want to judge?'))
-        result=evenOrOdd(number)
+        result=even_or_odd(number)
         print(result)
         menu()
     elif function=='3':
         number=int(input('What is the number you want to judge?'))
-        result=evenOrOdd(number)
+        result=even_or_odd(number)
         print(result)
         menu()
     elif function=='4':
         n=int(input('How many lines of asterisks do you want to print?'))
-        asterisks=printAsterisks(n)
+        asterisks=print_asterisks(n)
         print(asterisks)
         menu()
     elif function=='5':
         num=int(input('What is the number to be raised?'))
         exponent=int(input('What is the exponent?'))
-        result_number=raisePower(num,exponent)
+        result_number=raise_power(num,exponent)
         print('The result is',result_number)
         menu()
     elif function=='6':
         print('Done.') #finish the function
+    else:
+        print("Enter a number between 1 and 6")
+        menu()
 menu()
